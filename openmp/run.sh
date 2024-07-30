674b6c67f18e57fb0eb03ad72614137d8929ed2c
prakash <prakash.mohan@nrel.gov>
2024-07-30 12:12:00 -0500

    add a simple run script

---
+++ openmp/run.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+CC=gcc-13
+CFLAGS=-fopenmp
+# echo $CC $CFLAGS $1
+cmd $CC $CFLAGS $1
+for i in 1 2 4 8
+    do OMP_NUM_THREADS=$i ./a.out
+done
